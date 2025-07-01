324e94475c87fac5f7cfb157c685ece243bf1e0f
shubhamjaiminy <shubhamjaiminy.ee.aiet@gmail.com>
2025-07-01 15:04:33 +0530

    added some code

--- tests/page-objects/mobilePage.ts
+++ tests/page-objects/mobilePage.ts
@@ -6,6 +6,8 @@ export class mobile {
 
     async mobileSeries() {
         await this.page.locator('[placeholder="Search for Products, Brands and More"]').type('Mobiles');
+        await this.page.keyboard.press('Enter');
+        await this.page.waitForTimeout(5000);// Wait for 2 seconds to ensure the search results are loaded.
         // Expect a title "to contain" a substring.
     }
 }
